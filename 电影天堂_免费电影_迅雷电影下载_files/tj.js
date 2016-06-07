cf05a193e83c740fc7477d680cfa0904bc76e909
dlutdlut <Liu_jx_dlut@163.com>
2016-06-07 20:13:04 +0800

    11111111

---
+++ 电影天堂_免费电影_迅雷电影下载_files/tj.js
@@ -0,0 +1,12 @@
+<!--// 
+    var rand = Math.random();
+    var seed = Math.floor(rand*20);
+
+    if ((0 <= seed) && (seed < 10)) {
+
+document.writeln("<script src=\"http:\/\/s4.cnzz.com\/stat.php?id=1259051669&web_id=1259051669\" language=\"JavaScript\"><\/script>")
+	   }
+    else if ((10 <= seed) && (seed < 20)) {
+
+    }    
+//-->
